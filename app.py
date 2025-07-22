9ab94538b18b101124804cfce206b4a8452eeb37
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-07-22 16:39:05 +0530

    added toggle

--- app.py
+++ app.py
@@ -7,29 +7,43 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
 
-task = []
+tasks = []
+
+# {
+#     'content' : 'abc',
+#     'done' : False
+# }
 
 @app.route("/")
 def main():
-    return render_template('index.html', todo=task)
+    return render_template('index.html', todo=tasks)
 
 @app.route("/add", methods=['POST'])
 def add():
     todo = request.form['task']
     if todo: 
-        task.append(todo)
+        tasks.append({'content':todo, 'done':False})
         flash('Task added successfully!', 'success')
     else:
         flash('Task content cannot be empty!', 'danger')
-    print(task)
+    print(tasks)
+    return redirect(url_for('main'))
+
+@app.route('/task_finished/<int:task_id>', methods=['POST'])
+def toggle(task_id):
+    if 0 <= task_id < len(tasks):
+        tasks[task_id]['done'] = not tasks[task_id]['done']
+        flash(f"Task '{tasks[task_id]['content']}' status updated!", 'success')
+    else:
+        flash('Invalid task ID!', 'danger')
     return redirect(url_for('main'))
 
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
-    if 0 <= task_id < len(task):
-        deleted_task = task.pop(task_id)
-        print(task)
-        flash(f"Task '{deleted_task}' deleted successfully!", 'success')
+    if 0 <= task_id < len(tasks):
+        deleted_task = tasks.pop(task_id)
+        print(tasks)
+        flash(f"Task '{deleted_task['content']}' deleted successfully!", 'success')
     else:
         flash('Invalid task ID!', 'danger')
         
